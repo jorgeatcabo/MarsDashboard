5ae3746cbdf30428e1938bfa6511c3dea285fa80
El Roble <san_lucas2005@yahoo.com>
2022-08-22 14:09:03 -0600

    Add curiosity endpoint

--- src/server/index.js
+++ src/server/index.js
@@ -15,14 +15,27 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 // your API calls
 
 // example API call
+///https://github.com/chrisccerami/mars-photo-api
 app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+    try {        
+        let image = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ image })
     } catch (err) {
         console.log('error:', err);
     }
 })
+
+
+app.get('/curiosity', async (req, res) => {
+    try {
+        let results = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send(results)
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
